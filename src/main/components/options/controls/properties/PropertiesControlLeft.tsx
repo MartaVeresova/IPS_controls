b18e1d73062c3eca4e5a98f96c3029748028548b
MartaVeresova <MartaVeresovaa@gmail.com>
2022-01-12 18:49:21 +0300

    editing onImageDoubleClick, onSizeDoubleClick

--- src/main/components/options/controls/properties/PropertiesControlLeft.tsx
+++ src/main/components/options/controls/properties/PropertiesControlLeft.tsx
@@ -14,29 +14,29 @@ export const PropertiesControlLeft: FC<PropsType> = observer(props => {
     const {field, additionalModel} = props
 
 
-    const onImageClick = () => {
-        if (field.propertyValue) {
+    const onImageDoubleClick = () => {
+        if (field.propertyName === 'Изображение' && field.propertyValue) {
             additionalModel.setIsImageFieldExpanded(!additionalModel.isImageFieldExpanded)
             additionalModel.setIsSizeFieldExpanded(false)
         }
     }
-    const onSizeClick = () => {
+    const onSizeDoubleClick = () => {
         additionalModel.setIsSizeFieldExpanded(!additionalModel.isSizeFieldExpanded)
     }
 
 
     return (
         <div className={style.propertyName}>
-            <div className={style.imageField} tabIndex={0} onDoubleClick={onImageClick}>
+            <div className={style.imageField} tabIndex={0} onDoubleClick={onImageDoubleClick}>
                 {field.propertyName === 'Изображение' && (field.propertyValue) &&
-                    <Pointer isFieldExpanded={additionalModel.isImageFieldExpanded} onIconClick={onImageClick}
+                    <Pointer isFieldExpanded={additionalModel.isImageFieldExpanded} onIconClick={onImageDoubleClick}
                              type="imageFieldIcon"/>}
                 <div className={style.propertyNameField}>{field.propertyName}</div>
             </div>
             {field.propertyName === 'Изображение' &&
                 <div className={style.additionalFields} hidden={!additionalModel.isImageFieldExpanded}>
-                    <div className={style.sizeField} tabIndex={0} onDoubleClick={onSizeClick}>
-                        <Pointer isFieldExpanded={additionalModel.isSizeFieldExpanded} onIconClick={onSizeClick}
+                    <div className={style.sizeField} tabIndex={0} onDoubleClick={onSizeDoubleClick}>
+                        <Pointer isFieldExpanded={additionalModel.isSizeFieldExpanded} onIconClick={onSizeDoubleClick}
                                  type="sizeFieldIcon"/>
                         Size
                     </div>
